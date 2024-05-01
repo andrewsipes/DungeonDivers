75087b12083d56c104db3094407293c8b7edd438
andrewsipes <andrew.sipes@outlook.com>
2024-05-01 17:25:26 -0400

    bug fix for enemy bullets

--- Example Space Game/Source/Application.cpp
+++ Example Space Game/Source/Application.cpp
@@ -74,7 +74,7 @@ bool Application::Run() {
 	gamePlayManager gpManager(currentLevel, game);
 	PlayerStats playerStats(*gameConfig);
 
-	gpManager.AddEntities();
+	while(!gpManager.AddEntities());
 	gpManager.AddSystems(currentLevel, game, gameConfig, gInput, bufferedInput, gamePads, audioEngine, eventPusher, &playerStats, &rendererManager);
 
 	//Music Declaration and play main menu
@@ -96,7 +96,8 @@ bool Application::Run() {
 		currentLevel->Update(game, currentLevel);
 		currentLevel->postProcessing(&time, timeReset);
 
-		if (!rendererManager.mainMenuHUD->render &&!rendererManager.pauseMenu->render && !rendererManager.isPauseMenuRendered && !rendererManager.gameOverMenu->render)
+		if (!rendererManager.playerHUD->levelCompleteText->render &&!rendererManager.mainMenuHUD->render &&
+			!rendererManager.pauseMenu->render && !rendererManager.isPauseMenuRendered && !rendererManager.gameOverMenu->render)
 			GameLoop();
 
 		glClearColor(clr[0], clr[1], clr[2], clr[3]);
